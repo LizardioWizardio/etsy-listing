56131c1c0025e8a4bb0d4570d8ea583b8d215099
Le Moh Carman <dthgrz@gmail.com>
2022-12-22 12:47:05 +0700

    clsx added

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import clsx from 'clsx';
 
 const ItemCard = ({img, url, title, currency, price, quantity}) => {
     const formatTitle = (title) => {
@@ -15,32 +16,33 @@ const ItemCard = ({img, url, title, currency, price, quantity}) => {
         else return price + " " + currency
     }
 
-    const getQuantityIndicatorColor = (quantity) => {
-        if (quantity <= 10) {
-            return "bg-red-600 "
-        }
-        else if (quantity <= 20) {
-            return "bg-yellow-400 "
-        }
-        else return "bg-green-700 "
-    }
-
     if (!title || !price || !currency) {
         return null;
     }
 
+    const quantityIndicatorStyles = clsx('text-xl','font-bold','text-white','p-2','shadow-md','rounded-lg', {
+            'bg-red-600' : quantity <= 10,
+            'bg-yellow-400': quantity > 10 && quantity <= 20,
+            'bg-green-700': quantity > 20
+        }
+    );
+
     return (
-        <div className="max-w-2xl mx-auto m-1">
-            <div className="bg-white shadow-md rounded-lg max-w-sm dark:bg-gray-800 dark:border-gray-700">
+        <div className={clsx('max-w-2xl', 'mx-auto', 'm-1')}>
+            <div className={clsx('bg-white','shadow-md', 'rounded-lg', 'max-w-sm')}>
                 <a href={url}>
-                    <img className="rounded-t-lg p-8" src={img}
+                    <img className={clsx('rounded-t-lg', 'p-8')} src={img}
                          alt="product" />
                 </a>
-                <div className="px-5 pb-5">
-                    <h3 className="text-gray-900 text-sm tracking-tight p-1">{formatTitle(title)}</h3>
-                    <div className="flex items-center justify-between">
-                        <span className="text-2xl text-green-800 font-bold text-gray-900">{formatPrice(price, currency)}</span>
-                        <span className={getQuantityIndicatorColor(quantity) + "text-xl font-bold text-white p-2 shadow-md rounded-lg"}>{quantity + " left"}</span>
+                <div className={clsx('px-5', 'pb-5')}>
+                    <h3 className={clsx('text-gray-900', 'text-sm', 'tracking-tight', 'p-1')}>
+                        {formatTitle(title)}
+                    </h3>
+                    <div className={clsx('flex', 'items-center', 'justify-between')}>
+                        <span className={clsx('text-2xl', 'text-green-800', 'font-bold', 'text-gray-900')}>
+                            {formatPrice(price, currency)}
+                        </span>
+                        <span className={quantityIndicatorStyles}>{quantity + " left"}</span>
                     </div>
                 </div>
             </div>
